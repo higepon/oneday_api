ce7b88a6b4e5997501c212a99f9bcb9484f72c45
Taro Minowa Higepon <higepon@gmail.com>
2014-03-13 21:59:25 +0900

    Added test for invalid email address.

--- spec/controllers/api/registrations_controller_spec.rb
+++ spec/controllers/api/registrations_controller_spec.rb
@@ -15,19 +15,27 @@ describe Api::RegistrationsController do
         json = JSON.parse(response.body)
         expect(json['errors'].size).to be > 0
         expect(json['errors']['password'][0]).to eq('is too short (minimum is 8 characters)')
-        pp json
       end
     end
 
     context 'when account already exists' do
       it 'should return error' do
         post :create, { :user => { :email => 'user001@gmail.com', :password => 'abcdefghijklmno' } }
-        pp response.body
         expect(response).not_to be_success
         json = JSON.parse(response.body)
         expect(json['errors'].size).to be > 0
         expect(json['errors']['email'][0]).to eq('has already been taken')
-        pp json
+      end
+    end
+
+    context 'when invalid email is geven' do
+      it 'should return error' do
+        post :create, { :user => { :email => 'user001', :password => 'abcdefghijklmno' } }
+        pp response.body
+        expect(response).not_to be_success
+        json = JSON.parse(response.body)
+        expect(json['errors'].size).to be > 0
+        expect(json['errors']['email'][0]).to eq('is invalid')
       end
     end
 
