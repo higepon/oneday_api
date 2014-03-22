5b7d8ab26791e4325c19c710bf9d8d158d479eaa
Taro Minowa Higepon <higepon@gmail.com>
2014-03-22 09:19:50 -0700

    (Red) Added falling test.

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,2 +1,6 @@
 class Api::MessagesController < ApplicationController
+
+  def index
+    return render :json => { }
+  end
 end
--- spec/controllers/api/messages_controller_spec.rb
+++ spec/controllers/api/messages_controller_spec.rb
@@ -6,4 +6,16 @@ describe Api::MessagesController do
     request.accept = 'application/json'
     request.env['devise.mapping'] = Devise.mappings[:user]
   end
+
+  describe '#index' do
+    context 'when valid room is given' do
+      it 'should return messages in room' do
+        get :index, { :room_id => 1 }
+        puts response.body
+        expect(response).to be_success
+#        expect(json['success']).to eq(true)
+#        expect(json['user']['authentication_token'].size).to be > 0
+      end
+    end
+  end
 end
