55057664097e786ec571ae6e43e1971dc8cdd406
Taro Minowa Higepon <higepon@gmail.com>
2014-05-26 11:59:57 -0700

    smtp

---
+++ config/initializers/smtp.rb
@@ -0,0 +1,13 @@
+ctionMailer::Base.default_url_options = { host: Settings.default_host }
+
+if Settings.try(:mail)
+  ActionMailer::Base.smtp_settings = {
+    address:              Settings.mail.try(:address),
+    port:                 Settings.mail.try(:port),
+    domain:               Settings.mail.try(:domain),
+    user_name:            Settings.mail.try(:user_name),
+    password:             Settings.mail.try(:password),
+    authentication:       :plain,
+    enable_starttls_auto: true
+  }
+end
